8c06e83bc8973535e78bf5afe1f2ab00d6c686e7
ilyassdaaif <ilyass.cougar@gmail.com>
2024-08-01 21:22:41 +0000

    a

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""takes a string str1 and a string str2 as arguments and returns a concatenated string"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings and return the result.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenation of str1 and str2.
+    """
+    return str1 + str2

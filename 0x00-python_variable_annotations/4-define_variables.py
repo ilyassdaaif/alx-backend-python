2d345ba6e0fbf0851899525e2963aeeaf4872428
ilyassdaaif <ilyass.cougar@gmail.com>
2024-08-02 01:47:49 +0000

    s

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# An integer variable with a value of 1
+a: int = 1
+
+# A float variable representing an approximation of pi
+pi: float = 3.14
+
+# A bloolean variable indicating understanding of annotations
+i_understand_annotations: bool = True
+
+# A string variable containing the name of the school
+school: str = "Holberton"

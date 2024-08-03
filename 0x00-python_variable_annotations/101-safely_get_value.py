de40ea10db773ac2a874c3bef120951cf756a7c5
ilyassdaaif <ilyass.cougar@gmail.com>
2024-08-03 00:03:05 +0000

    a

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -11,7 +11,7 @@ def safely_get_value(
     dct: Mapping,
     key: Any,
     default: Union[T, None] = None
-)-> Union[Any, T]:
+) -> Union[Any, T]:
     """
     Safely get a value from a dictionary.
 

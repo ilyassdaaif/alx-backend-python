08773560c0ca744d8242a8d7b07a14b05b2762ad
ilyassdf <ilyassdf@gmail.com>
2024-08-23 21:54:01 +0000

    f

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -23,6 +23,23 @@ class TestGithubOrgClient(unittest.TestCase):
             f"https://api.github.com/orgs/{org_name}"
         )
 
+    def test_public_repos_url(self):
+        """
+        Test that the result of _public_repos_url is the expected one
+        based on the mocked payload.
+        """
+        known_payload = {
+                "repos_url": "https://api.github.com/orgs/testorg/repos"
+        }
+
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=unittest.mock.PropertyMock,
+                          return_value=known_payload) as mock_org:
+            client = GithubOrgClient("testorg")
+            url = client._public_repos_url
+            self.assertEqual(url, known_payload["repos_url"])
+            mock_org.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
